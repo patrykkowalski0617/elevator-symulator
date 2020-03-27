af588abca632956392d5f47f908c4b9574a3de6c
Patryk <patrykkowalski0617@gmail.com>
2020-03-27 22:21:11 +0100

    create elevator shaft

--- src/components/Building.js
+++ src/components/Building.js
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 import { BuildingContext } from "../context/BuildingContext";
 import Floors from "./Floors";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const BuildingStyled = styled.div`
     height: 70vh;
@@ -18,6 +18,8 @@ const BuildingStyled = styled.div`
     margin-top: calc(15vh + 25px);
     background: #555;
     position: relative;
+    display: flex;
+    flex-flow: row wrap;
 `;
 
 const Roof = styled.div`
@@ -39,6 +41,28 @@ const Roof = styled.div`
         position: absolute;
         bottom: 100%;
     }
+`;
+
+const FloorsCommonCss = css`
+    width: 40%;
+`;
+
+const EnterFloors = styled.div`
+    ${FloorsCommonCss}
+    border-width: 0 1px 0 2px;
+    border-style: solid;
+`;
+const ExitFloors = styled.div`
+    ${FloorsCommonCss}
+    border-width: 0 2px 0 1px;
+    border-style: solid;
+`;
+
+const ElevatorShaft = styled.div`
+    background: #777;
+    width: 10%;
+    border-width: 0 1px;
+    border-style: solid;
 `;
 
 function useElementSize(el) {
@@ -78,10 +102,20 @@ export default function Building() {
     return (
         <BuildingStyled ref={buildingRef}>
             <Roof text={`Building size: ${width} x ${buildingHeight}`}></Roof>
-            <Floors
-                numberOfFloors={numberOfFloors}
-                buildingHeight={buildingHeight}
-            ></Floors>
+            <EnterFloors>
+                <Floors
+                    numberOfFloors={numberOfFloors}
+                    buildingHeight={buildingHeight}
+                ></Floors>
+            </EnterFloors>
+            <ElevatorShaft></ElevatorShaft>
+            <ElevatorShaft></ElevatorShaft>
+            <ExitFloors>
+                <Floors
+                    numberOfFloors={numberOfFloors}
+                    buildingHeight={buildingHeight}
+                ></Floors>
+            </ExitFloors>
         </BuildingStyled>
     );
 }
--- src/components/Floor.js
+++ src/components/Floor.js
@@ -1,5 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const FloorStyled = styled.div`
+    border-bottom: 4px solid;
+`;
 
 export default function Floor({ buildingHeight, numberOfFloors }) {
-    return <div style={{ height: buildingHeight / numberOfFloors }}></div>;
+    return (
+        <FloorStyled
+            style={{ height: buildingHeight / numberOfFloors }}
+        ></FloorStyled>
+    );
 }
