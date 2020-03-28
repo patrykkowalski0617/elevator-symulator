8ed8d0436e67b17a6734384ec403ff5da77be715
Patryk <patrykkowalski0617@gmail.com>
2020-03-28 15:51:34 +0100

    state for elevator move modes

--- src/components/Elevators.js
+++ src/components/Elevators.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const ElevatorShaft = styled.div`
@@ -9,7 +9,7 @@ const ElevatorShaft = styled.div`
     position: relative;
 `;
 
-const Elevator = styled.div`
+const ElevatorStyled = styled.div`
     height: ${props => props.floorHeight}px;
     background: #999;
     position: absolute;
@@ -17,7 +17,35 @@ const Elevator = styled.div`
     width: 100%;
     border-width: 1px 0;
     border-style: solid;
+    transition: 5s linear;
+    ${props => (props.moveMode === 1 ? "bottom: 100%" : "")}
 `;
+
+const Elevator = ({ floorHeight }) => {
+    const [moveMode, setMoveMode] = useState(0); // 0 - no move, 1 - speed up, 2 - speed constans, 3 - slow down
+
+    const moveManager = () => {
+        let _moveMode = moveMode;
+        if (_moveMode < 3) {
+            _moveMode++;
+        } else {
+            _moveMode = 0;
+        }
+
+        setMoveMode(_moveMode);
+    };
+    return (
+        <ElevatorStyled floorHeight={floorHeight} moveMode={moveMode}>
+            <button
+                onClick={e => {
+                    moveManager();
+                }}
+            >
+                move mode: {moveMode}
+            </button>
+        </ElevatorStyled>
+    );
+};
 
 const Elevators = ({ floorHeight, numberOfElevators, elevatorWidth }) => {
     const renderElevators = () => {
