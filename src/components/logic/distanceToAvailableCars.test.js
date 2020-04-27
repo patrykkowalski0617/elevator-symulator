a6f33d6ba2d0ae2e447b2c10fef0cb041cde15c8
patrykkowalski0617 <patrykkowalski0617@gmail.com>
2020-04-27 13:14:51 +0200

    create distanceToAvailableCars.js and theNearestCar.js with tests

--- src/components/logic/distanceToAvailableCars.test.js
+++ src/components/logic/distanceToAvailableCars.test.js
@@ -1,66 +1,64 @@
 import distanceToAvailableCars from "./distanceToAvailableCars";
 
-(() => {
-    const cases = [
+const cases = [
+    [
+        ["ready", "ready"],
+        [0, 0],
+        4,
         [
-            ["ready", "ready"],
-            [0, 0],
-            4,
-            [
-                { carId: 0, distance: 4 },
-                { carId: 1, distance: 4 }
-            ]
-        ],
+            { carId: 0, distance: 4 },
+            { carId: 1, distance: 4 }
+        ]
+    ],
+    [
+        ["ready", "ready", null],
+        [0, 0, 0],
+        4,
         [
-            ["ready", "ready", null],
-            [0, 0, 0],
-            4,
-            [
-                { carId: 0, distance: 4 },
-                { carId: 1, distance: 4 }
-            ]
-        ],
-        [["ready", "ready", null], [0, 0], 4, []],
-        [[], [], 4, []],
-        [[], [0, 0], 4, []],
+            { carId: 0, distance: 4 },
+            { carId: 1, distance: 4 }
+        ]
+    ],
+    [["ready", "ready", null], [0, 0], 4, []],
+    [[], [], 4, []],
+    [[], [0, 0], 4, []],
+    [
+        ["go-up", "ready"],
+        [0, 0],
+        4,
         [
-            ["go-up", "ready"],
-            [0, 0],
-            4,
-            [
-                { carId: 0, distance: 4 },
-                { carId: 1, distance: 4 }
-            ]
-        ],
-        [["go-up", "ready"], [3, 0], 4, [{ carId: 1, distance: 4 }]],
-        [["go-down", "go-up"], [1, 1], 4, [{ carId: 1, distance: 3 }]],
+            { carId: 0, distance: 4 },
+            { carId: 1, distance: 4 }
+        ]
+    ],
+    [["go-up", "ready"], [3, 0], 4, [{ carId: 1, distance: 4 }]],
+    [["go-down", "go-up"], [1, 1], 4, [{ carId: 1, distance: 3 }]],
+    [
+        ["go-up", "go-down"],
+        [0, 6],
+        4,
         [
-            ["go-up", "go-down"],
-            [0, 6],
-            4,
-            [
-                { carId: 0, distance: 4 },
-                { carId: 1, distance: 2 }
-            ]
-        ],
-        [["go-up", "go-down"], [0, 5], 4, [{ carId: 0, distance: 4 }]]
-    ];
+            { carId: 0, distance: 4 },
+            { carId: 1, distance: 2 }
+        ]
+    ],
+    [["go-up", "go-down"], [0, 5], 4, [{ carId: 0, distance: 4 }]]
+];
 
-    for (let i = 0; i < cases.length; i++) {
-        const [
-            allCarStates,
-            allCarsCurrentFloor,
-            floorNumber,
-            expectedReturn
-        ] = cases[i];
-        test("case " + i, () => {
-            expect(
-                distanceToAvailableCars(
-                    allCarStates,
-                    allCarsCurrentFloor,
-                    floorNumber
-                )
-            ).toEqual(expectedReturn);
-        });
-    }
-})();
+for (let i = 0; i < cases.length; i++) {
+    const [
+        allCarStates,
+        allCarsCurrentFloor,
+        floorNumber,
+        expectedReturn
+    ] = cases[i];
+    test("case " + i, () => {
+        expect(
+            distanceToAvailableCars(
+                allCarStates,
+                allCarsCurrentFloor,
+                floorNumber
+            )
+        ).toEqual(expectedReturn);
+    });
+}
